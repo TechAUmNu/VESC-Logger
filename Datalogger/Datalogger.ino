505612f90dc7827c6be6801be9a59f7e0e1d9fdb
Euan Mutch <euan.mutch@gmail.com>
2022-04-13 18:56:45 +0100

    Increase datarate, remove some fields.

--- Datalogger/Datalogger.ino
+++ Datalogger/Datalogger.ino
@@ -25,21 +25,22 @@ File logFile, infoFile;
 
 void setup() {
   pinMode(LED, OUTPUT);
+  
   // see if the card is present and can be initialized:
 
-  while (!SD.begin(chipSelect));
+  while (!SD.begin(chipSelect)){}
 
   logFile = SD.open("log.csv", FILE_WRITE);
   infoFile = SD.open("fw.txt", FILE_WRITE);
-
+  
   // Once SDCard is ok, connect to VESC
-  Serial.begin(460800);
-  while (!Serial) {
-    ;
-  }
+  Serial.begin(1000000);
+  while (!Serial) {}
+  
   UART.setSerialPort(&Serial);
 
   // Read firmware info and log
+  while(!UART.getVescFirmwareInfo()){}
   if ( UART.getVescFirmwareInfo() ) {
     if (infoFile) {
       infoFile.print("FW:");
@@ -53,65 +54,51 @@ void setup() {
       infoFile.close();
     }
   }
-  logFile.println("ms_today,input_voltage,temp_mos_max,temp_mos_1,temp_mos_2,temp_mos_3,temp_motor,current_motor,current_in,d_axis_current,q_axis_current,erpm,duty_cycle,amp_hours_used,amp_hours_charged,watt_hours_used,watt_hours_charged,tachometer,tachometer_abs,encoder_position,fault_code,vesc_id,d_axis_voltage,q_axis_voltage");
+ 
+  logFile.println("ms_today,input_voltage,temp_mos_max,temp_mos_1,temp_mos_2,temp_mos_3,temp_motor,current_motor,current_in,d_axis_current,q_axis_current,erpm,duty_cycle,encoder_position,fault_code,vesc_id,d_axis_voltage,q_axis_voltage");
+  digitalWrite(LED, HIGH);
 }
 
-void loop() {
-  if (logFile) {
-    if ( UART.getVescValues() ) {
-      digitalWrite(LED, HIGH);
-      logFile.print(millis()); // Timestamp
-      logFile.print(",");
-      logFile.print(UART.data.input_voltage);
-      logFile.print(",");
-      logFile.print(UART.data.temp_mos_max);
-      logFile.print(",");
-      logFile.print(UART.data.temp_mos_1);
-      logFile.print(",");
-      logFile.print(UART.data.temp_mos_2);
-      logFile.print(",");
-      logFile.print(UART.data.temp_mos_3);
-      logFile.print(",");
-      logFile.print(UART.data.temp_motor);
-      logFile.print(",");
-      logFile.print(UART.data.current_motor);
-      logFile.print(",");
-      logFile.print(UART.data.current_in);
-      logFile.print(",");
-      logFile.print(UART.data.d_axis_current);
-      logFile.print(",");
-      logFile.print(UART.data.q_axis_current);
-      logFile.print(",");
-      logFile.print(UART.data.erpm);
-      logFile.print(",");
-      logFile.print(UART.data.duty_cycle);
-      logFile.print(",");
-      logFile.print(UART.data.amp_hours_used);
-      logFile.print(",");
-      logFile.print(UART.data.amp_hours_charged);
-      logFile.print(",");
-      logFile.print(UART.data.watt_hours_charged);
-      logFile.print(",");
-      logFile.print(UART.data.tachometer);
-      logFile.print(",");
-      logFile.print(UART.data.tachometer_abs);
-      logFile.print(",");
-      logFile.print(UART.data.encoder_position);
-      logFile.print(",");
-      logFile.print(UART.data.fault_code);
-      logFile.print(",");
-      logFile.print(UART.data.vesc_id);
-      logFile.print(",");
-      logFile.print(UART.data.d_axis_voltage);
-      logFile.print(",");
-      logFile.println(UART.data.q_axis_voltage);
-      logFile.availableForWrite();
-    }
-  } else {
-    digitalWrite(LED, LOW);
-    while (!SD.begin(chipSelect)) {
-      digitalWrite(LED, LOW);
-    }
-    logFile = SD.open("log.csv", FILE_WRITE);
-  }
+void loop() {  
+    if ( UART.getVescValues() ) {        
+        logFile.print(millis()); // Timestamp
+        logFile.print(",");
+        logFile.print(UART.data.input_voltage);
+        logFile.print(",");
+        logFile.print(UART.data.temp_mos_max);
+        logFile.print(",");
+        logFile.print(UART.data.temp_mos_1);
+        logFile.print(",");
+        logFile.print(UART.data.temp_mos_2);
+        logFile.print(",");
+        logFile.print(UART.data.temp_mos_3);
+        logFile.print(",");
+        logFile.print(UART.data.temp_motor);
+        logFile.print(",");
+        logFile.print(UART.data.current_motor);
+        logFile.print(",");
+        logFile.print(UART.data.current_in);
+        logFile.print(",");
+        logFile.print(UART.data.d_axis_current);
+        logFile.print(",");
+        logFile.print(UART.data.q_axis_current);
+        logFile.print(",");
+        logFile.print(UART.data.erpm);
+        logFile.print(",");
+        logFile.print(UART.data.duty_cycle);
+        logFile.print(",");      
+        logFile.print(UART.data.encoder_position);
+        logFile.print(",");
+        logFile.print(UART.data.fault_code);
+        logFile.print(",");
+        logFile.print(UART.data.vesc_id);
+        logFile.print(",");
+        logFile.print(UART.data.d_axis_voltage);
+        logFile.print(",");
+        logFile.println(UART.data.q_axis_voltage);
+        logFile.availableForWrite();
+        digitalWrite(LED, HIGH);       
+    } else {
+      digitalWrite(LED, LOW);  
+    }    
 }
