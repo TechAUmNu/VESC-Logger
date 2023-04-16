22adee17c05a2f252c0f9bdfbe40f5db25603d89
Euan <euan.mutch@gmail.com>
2023-04-16 21:33:38 +0100

    Add hardware protection, fix overflow on line counter

--- Datalogger/Datalogger.ino
+++ Datalogger/Datalogger.ino
@@ -2,11 +2,11 @@
   SD card datalogger
   SD Card must be formatted FAT32, exFAT will not work at the moment
   created  23 Feb 2022
-  modified 11 Apr 2022
+  modified 16 Apr 2023
   by Euan Mutch (TechAUmNu)
 
 */
-#define MAX_LINES_PER_FILE 180000 // approx 30 min - Stops VESC Tool crashing when opening very long logs
+#define MAX_LINES_PER_FILE 50000 // Stops VESC Tool crashing when opening very long logs
 
 #include <EEPROM.h>
 #include <SPI.h>
@@ -15,6 +15,7 @@
 #include <Comparator.h>
 #include <Logic.h>
 #include <Event.h>
+#include <Wire.h>
 
 const int LED = 4;
 const int chipSelect = 13;
@@ -29,155 +30,167 @@ void setup() {
   pinMode(LED, OUTPUT);
   pinMode(cardDetect, INPUT);
   digitalWrite(LED, 1);
-  
-  // Configure event channel for reset
-  EVSYS.ASYNCCH1 = EVSYS_ASYNCCH1_PORTB_PIN2_gc;      // Use CCL LUT1 as event generator
-  EVSYS.ASYNCUSER3 = EVSYS_ASYNCUSER3_ASYNCCH1_gc;  // ASYNCUSER4 is LUT1 event 0
-   
+
+  // Event channel for reseting faults
+  Event1.set_generator(gens::disable);
+  Event1.set_user(user::ccl1_event_a);
+  Event1.start();
+
   // Initialize logic block 1
   Logic1.enable = true;                // Enable logic block 1
   Logic1.input0 = logic_in::event_a;         // Connect input 0 to ccl1_event_a (PB2 through Event1)
-  Logic1.input1 = logic_in::masked;         
-  Logic1.input2 = logic_in::masked;          
-  Logic1.output = logic_out::disable;  
+  Logic1.input1 = logic_in::masked;
+  Logic1.input2 = logic_in::masked;
+  Logic1.output = logic_out::disable;
   Logic1.truth = 0xFE;                  // Set truth table
   Logic1.init();
-  
+
   // Configure relevant comparator parameters
   Comparator0.input_p = comparator_in_p::in2;      // Use positive input 2 (PB1)
   Comparator1.input_p = comparator_in_p::in3;      // Use positive input 3 (PB4)
-  Comparator2.input_p = comparator_in_p::in1;      // Use positive input 1 (PB0)  
+  Comparator2.input_p = comparator_in_p::in1;      // Use positive input 1 (PB0)
 
   // Initialize comparators
   Comparator0.init();
   Comparator1.init();
   Comparator2.init();
-  
+
   // Configure logic block
-  Logic0.enable = true;               
-  Logic0.input0 = logic_in::ac0;   
-  Logic0.input1 = logic_in::ac1;  
+  Logic0.enable = true;
+  Logic0.input0 = logic_in::ac0;
+  Logic0.input1 = logic_in::ac1;
   Logic0.input2 = logic_in::ac2;
-  Logic0.output = logic_out::enable; 
+  Logic0.output = logic_out::enable;
   Logic0.sequencer = logic_sequencer::rs_latch; // Latch output
   Logic0.truth = 0xFE;                    // Set truth table (3 input OR)
-
-  // Initialize logic block 0
   Logic0.init();
 
   // Start comparators
   Comparator0.start();
   Comparator1.start();
   Comparator2.start();
-  
+
   // Start the AVR logic hardware
   Logic::start();
 
-  Serial.swap(1);
+  // I2C for resetting overcurrent protection
+  Wire.swap(1);
+  Wire.begin(0x69);                 // join i2c bus with address 0x69 NICE
+  Wire.onReceive(resetOvercurrent);
+
+
+  //Serial.swap(1); // Not required on V4.1 logic board
   Serial.begin(1000000);
   while (!Serial) {}
-  UART.setSerialPort(&Serial); 
+  UART.setSerialPort(&Serial);
   SPI.swap(1);
-  
+
   // create new file each boot
   boots =  EEPROM.read(0);
-  EEPROM.update(0, ++boots); 
+  EEPROM.update(0, ++boots);
+}
+
+void resetOvercurrent(int16_t numBytes) {
+  if (Wire.read() == 0x53) { // Check for correct reset code
+    Event1.soft_event();
+  }
 }
 
 
 byte subFile = 0; // creates a new file each time sd card is replugged while in the same boot
 
-void loop() { 
-  while (digitalRead(cardDetect)){}   // Wait for card
-  while (!SD.begin(chipSelect)){}     // Wait for card to connect
+void loop() {
+  while (digitalRead(cardDetect)) {}  // Wait for card
+  while (!SD.begin(chipSelect)) {}    // Wait for card to connect
   char temp[10];
-  // Filename must conform to short DOS 8.3 
+  // Filename must conform to short DOS 8.3
   char fileName[20];
   strcpy(fileName, itoa(boots, temp, 10));
-  strcat(fileName, "-"); 
-  strcat(fileName, itoa(subFile, temp, 10));     
-  strcat(fileName, ".csv"); 
+  strcat(fileName, "-");
+  strcat(fileName, itoa(subFile, temp, 10));
+  strcat(fileName, ".csv");
   logFile = SD.open(fileName, FILE_WRITE);
   subFile++;
-  
-  
+
+
   // Read firmware info and put at start of log file
-  while(!UART.getVescFirmwareInfo()){
+  while (!UART.getVescFirmwareInfo()) {
     digitalWrite(LED, 1);
     delay(500);
     digitalWrite(LED, 0);
     delay(500);
-  }    
-  logFile.print("FW:");
-  logFile.print(UART.firmware.firmwareVersionMajor);
-  logFile.print(".");
-  logFile.print(UART.firmware.firmwareVersionMinor);
-  logFile.print(" B:");
-  logFile.println(UART.firmware.firmwareVersionBeta);
-  logFile.print("HW:");
-  logFile.println(UART.firmware.hardwareName);     
+  }
+  // Removed as it crashes vesc tool when opening log
+  //logFile.print("FW:");
+  //logFile.print(UART.firmware.firmwareVersionMajor);
+  //logFile.print(".");
+  //logFile.print(UART.firmware.firmwareVersionMinor);
+  //logFile.print(" B:");
+  //logFile.println(UART.firmware.firmwareVersionBeta);
+  //logFile.print("HW:");
+  //logFile.println(UART.firmware.hardwareName);
   logFile.println("ms_today,input_voltage,temp_mos_max,temp_mos_1,temp_mos_2,temp_mos_3,temp_motor,current_motor,current_in,d_axis_current,q_axis_current,erpm,duty_cycle,amp_hours_used,amp_hours_charged,watt_hours_used,watt_hours_charged,tachometer,tachometer_abs,encoder_position,fault_code,vesc_id,d_axis_voltage,q_axis_voltage");
-   
-  int lineCount = 0;
+
+  long lineCount = 0;
   bool ledState = false;
- 
+
   // Logging loop
-  while(true) {   
-    if(lineCount % 10 == 1) { // Flash LED to show logging
-      digitalWrite(LED, ledState=!ledState);
+  while (true) {
+    if (lineCount % 10 == 1) { // Flash LED to show logging
+      digitalWrite(LED, ledState = !ledState);
     }
-    if ( UART.getVescValues() ) {        
-        logFile.print(millis()); // Timestamp
-        logFile.print(",");
-        logFile.print(UART.data.input_voltage);
-        logFile.print(",");
-        logFile.print(UART.data.temp_mos_max);
-        logFile.print(",");
-        logFile.print(UART.data.temp_mos_1);
-        logFile.print(",");
-        logFile.print(UART.data.temp_mos_2);
-        logFile.print(",");
-        logFile.print(UART.data.temp_mos_3);
-        logFile.print(",");
-        logFile.print(UART.data.temp_motor);
-        logFile.print(",");
-        logFile.print(UART.data.current_motor);
-        logFile.print(",");
-        logFile.print(UART.data.current_in);
-        logFile.print(",");
-        logFile.print(UART.data.d_axis_current);
-        logFile.print(",");
-        logFile.print(UART.data.q_axis_current);
-        logFile.print(",");
-        logFile.print(UART.data.erpm);
-        logFile.print(",");
-        logFile.print(UART.data.duty_cycle);
-        logFile.print(",");
-        logFile.print(UART.data.amp_hours_used);
-        logFile.print(",");
-        logFile.print(UART.data.amp_hours_charged);
-        logFile.print(",,,,,");      
-        logFile.print(UART.data.encoder_position);
-        logFile.print(",");
-        logFile.print(UART.data.fault_code);
-        logFile.print(",");
-        logFile.print(UART.data.vesc_id);
-        logFile.print(",");
-        logFile.print(UART.data.d_axis_voltage);
-        logFile.print(",");
-        logFile.println(UART.data.q_axis_voltage);
-        logFile.availableForWrite();         
-        lineCount++;       
+    if ( UART.getVescValues() ) {
+      logFile.print(millis()); // Timestamp
+      logFile.print(",");
+      logFile.print(UART.data.input_voltage);
+      logFile.print(",");
+      logFile.print(UART.data.temp_mos_max);
+      logFile.print(",");
+      logFile.print(UART.data.temp_mos_1);
+      logFile.print(",");
+      logFile.print(UART.data.temp_mos_2);
+      logFile.print(",");
+      logFile.print(UART.data.temp_mos_3);
+      logFile.print(",");
+      logFile.print(UART.data.temp_motor);
+      logFile.print(",");
+      logFile.print(UART.data.current_motor);
+      logFile.print(",");
+      logFile.print(UART.data.current_in);
+      logFile.print(",");
+      logFile.print(UART.data.d_axis_current);
+      logFile.print(",");
+      logFile.print(UART.data.q_axis_current);
+      logFile.print(",");
+      logFile.print(UART.data.erpm);
+      logFile.print(",");
+      logFile.print(UART.data.duty_cycle);
+      logFile.print(",");
+      logFile.print(UART.data.amp_hours_used);
+      logFile.print(",");
+      logFile.print(UART.data.amp_hours_charged);
+      logFile.print(",,,,,");
+      logFile.print(UART.data.encoder_position);
+      logFile.print(",");
+      logFile.print(UART.data.fault_code);
+      logFile.print(",");
+      logFile.print(UART.data.vesc_id);
+      logFile.print(",");
+      logFile.print(UART.data.d_axis_voltage);
+      logFile.print(",");
+      logFile.println(UART.data.q_axis_voltage);
+      logFile.availableForWrite();
+      lineCount++;
     } else {
-        digitalWrite(LED, LOW);  
+      digitalWrite(LED, LOW);
     }
-    if(lineCount > MAX_LINES_PER_FILE){
-        logFile.close(); // Close the file in the file system
-        break;
-    }   
-    if (digitalRead(cardDetect)) {   
+    if (lineCount > MAX_LINES_PER_FILE) {
       logFile.close(); // Close the file in the file system
       break;
-    }    
+    }
+    if (digitalRead(cardDetect)) {
+      logFile.close(); // Close the file in the file system
+      break;
+    }
   }
 }
---
+++ HardwareProtection/HardwareProtection.ino
@@ -0,0 +1,246 @@
+/*
+  created  23 Feb 2022
+  modified 16 Apr 2023
+  by Euan Mutch (TechAUmNu)
+
+*/
+
+
+#include <Comparator.h>
+#include <Logic.h>
+#include <Event.h>
+#include <Wire.h>
+#include <movingAvg.h>
+
+
+
+#define RESTORE_3V3_DELAY 1000
+#define RESTORE_5V_DELAY 1000
+#define RESTORE_12V_DELAY 20000
+
+#define TRIP_3V3_DELAY 100
+#define TRIP_5V_DELAY 100
+#define TRIP_12V_DELAY 2000
+
+#define STARTUP_12V_DELAY 5000
+
+#define TRIP_12V_THRES 800 //11.3V
+
+const int PIN_LED = 10;
+const int PIN_3V3_EN = 7;
+const int PIN_3V3_FAULT = 6;
+const int PIN_5V_EN = 12;
+const int PIN_5V_FAULT = 13;
+const int PIN_P_GOOD = 11;
+const int PIN_12V_AUX_MON = 4;
+const int PIN_12V_AUX_EN = 16;
+
+static int F_3V3_count = 0;
+static int F_5V_count = 0;
+static int F_12V_count = 0;
+static int OK_12V_count = 0;
+static int F_PGOOD_count = 0;
+static int Startup_12V_Count = 0;
+static bool AUX_FAULTED = false;
+
+byte boots;
+movingAvg V_12V_AUX_MON(10);
+
+
+
+void setup() {
+  pinMode(PIN_LED, OUTPUT);
+  pinMode(PIN_3V3_EN, OUTPUT);
+  pinMode(PIN_5V_EN, OUTPUT);
+  pinMode(PIN_12V_AUX_EN, OUTPUT);
+  pinMode(PIN_3V3_FAULT, INPUT_PULLUP);
+  pinMode(PIN_5V_FAULT, INPUT_PULLUP);
+  pinMode(PIN_P_GOOD, INPUT_PULLUP);
+  pinMode(PIN_12V_AUX_MON, INPUT);
+
+  digitalWrite(PIN_LED, 1);
+
+  // Configure event channel for reset  
+  Event1.set_generator(gens::disable);
+  Event1.set_user(user::ccl1_event_a);
+  Event1.start();
+  
+  // Initialize logic block 1
+  Logic1.enable = true;                // Enable logic block 1
+  Logic1.input0 = logic_in::event_a;         // Connect input 0 to ccl1_event_a (PB2 through Event1)
+  Logic1.input1 = logic_in::masked;
+  Logic1.input2 = logic_in::masked;
+  Logic1.output = logic_out::disable;
+  Logic1.truth = 0xFE;                  // Set truth table
+  Logic1.init();
+
+  // Configure relevant comparator parameters
+  Comparator0.input_p = comparator_in_p::in2;      // Use positive input 2 (PB1)
+  Comparator1.input_p = comparator_in_p::in3;      // Use positive input 3 (PB4)
+  Comparator2.input_p = comparator_in_p::in1;      // Use positive input 1 (PB0)
+
+  // Initialize comparators
+  Comparator0.init();
+  Comparator1.init();
+  Comparator2.init();
+
+  // Configure logic block
+  Logic0.enable = true;
+  Logic0.input0 = logic_in::ac0;
+  Logic0.input1 = logic_in::ac1;
+  Logic0.input2 = logic_in::ac2;
+  Logic0.output = logic_out::enable;
+  Logic0.sequencer = logic_sequencer::rs_latch; // Latch output
+  Logic0.truth = 0xFE;                    // Set truth table (3 input OR)
+  Logic0.init();
+
+  // Start comparators
+  Comparator0.start();
+  Comparator1.start();
+  Comparator2.start();
+
+  // Start the AVR logic hardware
+  Logic::start();
+
+  // I2C for resetting overcurrent protection
+  Wire.swap(1);
+  Wire.begin(0x68);                 // join i2c bus with address 0x68
+  Wire.onReceive(resetOvercurrent);
+
+  digitalWrite(PIN_LED, 0);
+
+  // Turn on outputs that should be on all the time
+  digitalWrite(PIN_3V3_EN, 1);
+  digitalWrite(PIN_5V_EN, 1);
+  digitalWrite(PIN_12V_AUX_EN, 0);
+  V_12V_AUX_MON.begin();
+}
+
+static bool AUX_ENABLED = false;
+
+void resetOvercurrent(int16_t numBytes) {
+  switch (Wire.read()) {
+    case 0x54:
+      Event1.soft_event();
+      break;
+    case 0x21:
+      AUX_ENABLED = true;
+      Startup_12V_Count = STARTUP_12V_DELAY;
+      break;
+    case 0x20:
+      AUX_ENABLED = false;
+      break;
+  }
+}
+
+
+
+// Instantly trip outputs, retry after 1s.
+void loop() {
+  delay(1);
+  digitalWrite(PIN_LED, 0);
+  if (!digitalRead(PIN_3V3_FAULT))
+  {
+    if (F_3V3_count < TRIP_3V3_DELAY)
+    {
+      F_3V3_count++;
+    } else {
+      digitalWrite(PIN_3V3_EN, 0);
+      digitalWrite(PIN_LED, 1);
+      F_3V3_count = RESTORE_3V3_DELAY;
+    }
+  } else {
+    if (F_3V3_count > 0)
+    {
+      F_3V3_count--;
+      digitalWrite(PIN_LED, 1);
+    } else {
+      digitalWrite(PIN_3V3_EN, 1);
+    }
+  }
+
+  if (!digitalRead(PIN_5V_FAULT))
+  {
+    if (F_5V_count < TRIP_5V_DELAY)
+    {
+      F_5V_count++;
+    } else {
+      digitalWrite(PIN_5V_EN, 0);
+      digitalWrite(PIN_LED, 1);
+      F_5V_count = 1000;
+    }
+
+  } else {
+    if (F_5V_count > 0)
+    {
+      F_5V_count--;
+      digitalWrite(PIN_LED, 1);
+    } else {
+      digitalWrite(PIN_5V_EN, 1);
+    }
+  }
+
+  if (AUX_ENABLED)
+  {
+    // Only enable 12v aux if the output is enabled and 12V rail is OK.
+    if (!digitalRead(PIN_P_GOOD))
+    {
+      digitalWrite(PIN_LED, 1);
+      digitalWrite(PIN_12V_AUX_EN, 0);
+      F_PGOOD_count = 50;
+    } else {
+      if (F_PGOOD_count > 0)
+      {
+        F_PGOOD_count--;
+      } else {
+        if (V_12V_AUX_MON.reading(analogRead(PIN_12V_AUX_MON)) < 850)
+        {
+          if (AUX_FAULTED)
+          {
+            if(Startup_12V_Count <= 0)
+              digitalWrite(PIN_12V_AUX_EN, 0);
+            digitalWrite(PIN_LED, 1);
+            if (F_12V_count > 0)
+            {
+              F_12V_count--;
+            } else {
+              AUX_FAULTED = false;
+              Startup_12V_Count = STARTUP_12V_DELAY; // Allow things to start up again
+              digitalWrite(PIN_12V_AUX_EN, 1);
+            }
+          } else {
+            if (F_12V_count < TRIP_12V_DELAY)
+            {
+              F_12V_count++;
+            } else {
+              F_12V_count = RESTORE_12V_DELAY;
+              AUX_FAULTED = true;
+              if(Startup_12V_Count <= 0)
+                digitalWrite(PIN_12V_AUX_EN, 0);
+              digitalWrite(PIN_LED, 1);
+            }
+          }
+        } else {
+          //Windup protection
+          if (OK_12V_count < TRIP_12V_DELAY / 5)
+          {
+            OK_12V_count++;
+          } else {
+            OK_12V_count = 0;
+            F_12V_count = 0;
+          }
+        }
+      }
+      //Handle startup, allows high current start for fans etc.
+      if (Startup_12V_Count > 0)
+      {
+        Startup_12V_Count--;
+        digitalWrite(PIN_12V_AUX_EN, 1);
+      }
+    }
+
+    
+  } else {
+    digitalWrite(PIN_12V_AUX_EN, 0);
+  }
+}
